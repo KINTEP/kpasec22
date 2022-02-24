a3c702d489853cf4036d9c13989f1e59809511c9
Sir Newton <kuminewton@gmil.com>
2022-02-24 11:35:40 +0000

    changed

--- app.py
+++ app.py
@@ -365,6 +365,23 @@ def query_cash_book(start, end, df):
 	new1 = df[(df['date'] >= start) & (df['date'] <= end)]
 	return new1
 
+def student_ledg(date, id1):
+	con = create_engine(uri)
+	date = pd.to_datetime(date)
+    q1 = pd.read_sql_query(f"SELECT student_payments.date, fullname, etl_amount, pta_amount, tx_id, category from student INNER JOIN student_payments ON student_payments.student_id = student.id WHERE student.id_number = '{id1}'", con)
+    q2 = pd.read_sql_query("SELECT begin_date, etl, pta FROM charges", con)
+    q3 = q2[q2['begin_date'] >= date]
+    q3['category'] = 'charge'
+    q3.rename(columns={'etl':'etl_amount', 'pta':'pta_amount', 'begin_date':'date'}, inplace = True)
+    q3['etl_amount'] = -1*q3['etl_amount']
+    q3['pta_amount'] = -1*q3['pta_amount']
+    comb1 = pd.merge(q3, q1, how = 'outer')
+    comb1['etl_total'] = comb1['etl_amount'].cumsum()
+    comb1['pta_total'] = comb1['pta_amount'].cumsum()
+    df2 = comb1.sort_values('date', ignore_index=True)
+    df2.fillna(0, inplace=True)
+    return df2
+
 
 @app.route("/accountant_dashboard/cash_book_report1/<start>,<end>, <cat>")
 @login_required
@@ -802,22 +819,21 @@ def ledger_results(phone, dob):
 		idx = generate_student_id(phone1, dob)
 		student = Student.query.filter_by(id_number=idx).first()
 		if student:
-			charge = Charges.query.filter(Charges.begin_date >= student.date_admitted).all()
-			payments = db.session.query(StudentPayments).filter(and_(or_(StudentPayments.student_id==student.id,StudentPayments.category=='charge'), StudentPayments.date >= student.date_admitted)).all()
-			pmt2 = StudentPayments.query.filter_by(student_id=student.id)
-			etls = db.session.query(ETLIncome).filter(and_(or_(ETLIncome.student_id==student.id, ETLIncome.category=='charge'), ETLIncome.date >= student.date)).all()
-			ptas = db.session.query(PTAIncome).filter(and_(or_(PTAIncome.student_id==student.id, PTAIncome.category=='charge'), PTAIncome.date >= student.date)).all()
-			pta = [i.amount if i.category =="revenue" else -1*i.amount for i in ptas]
-			etl = [i.amount if i.category =="revenue" else -1*i.amount for i in etls]
+			df2 = student_ledg(date = student.date_admitted)
+			date = [str(i)[:10] for i in df2['date']]
+			ch1 = df2[df2['category'] == 'charge']
+			pta_charge = float(ch1['pta_amount'])
+			etl_charge = float(ch1['etl_amount'])
+			etls = list(df2['etl_amount'])
+			ptas = list(df2['pta_amount'])
+			category = list(df2['category'])
+			pta = [i for i in ptas if i > 0]
+			etl = [i for i in etls if i > 0]
 			total = sum(etl) + sum(pta)
-			cum1 = cumsum(etl)
-			cum2 = cumsum(pta)
-			if len(charge) > 0:
-				charge = charge[-1]
-			else:
-				charge = 0
-			return render_template("ledger_results1.html", student=student, payments=payments, cum1=cum1, cum2=cum2,
-			 charge=charge, etls=etls, ptas=ptas)
+			cum1 = cumsum(etls)
+			cum2 = cumsum(ptas)
+			return render_template("ledger_results1.html", student=student, cum1=cum1, cum2=cum2,
+			 pta_charge=pta_charge, etl_charge=etl_charge,etls=etls, ptas=ptas)
 		else:
 			flash("Student not found!", "danger")
 			return redirect(url_for('search_ledgers'))
@@ -1017,7 +1033,7 @@ class StudentPayments(db.Model):
 	semester = db.Column(db.String(10), nullable = False)
 	mode_of_payment = db.Column(db.String(20))
 	category = db.Column(db.String(20), default="revenue")
-	student_id = db.Column(db.Integer, db.ForeignKey('student.id'))#Here we reference the table name
+	student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
 
 	def __repr__(self):
 		return f'User: {self.student_id}'
